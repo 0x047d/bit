90fac29ed5fe7653f08dc3b0f406366a2777cc12
47d <47d@bitcoin.sh>
2017-03-24 22:06:29 -0500

    include: keccak.cpp <-> keccak-test.cpp

--- src/keccak.mk
+++ src/keccak.mk
@@ -4,10 +4,21 @@ keccak:
 keccak.test: keccak.test.build
 	$(KECCAK_TEST_EXE)
 
-keccak.test.build: keccak.test.clean
+keccak.test.build: keccak.test.clean $(OBJ)/keccak.o
 	$(CC) -std=$(STD)\
 	      -I$(INCLUDE) $(KECCAK_TEST_MAIN)\
+	      $(OBJ)/keccak.o\
 	      -o $(KECCAK_TEST_EXE)
 
+$(OBJ)/keccak.o:\
+	$(LIB)/keccak.cpp\
+	$(INCLUDE)/sph_keccak.h\
+	$(INCLUDE)/sph_types.h
+	$(CC) -std=$(STD)\
+	      -Wall -g\
+	      -I$(INCLUDE)\
+	      -c $(LIB)/keccak.cpp\
+	      -o $(OBJ)/keccak.o
+
 keccak.test.clean:
-	@-rm -rf $(KECCAK_TEST_EXE)
+	@-rm -rf $(KECCAK_TEST_EXE) $(OBJ)/keccak.o
